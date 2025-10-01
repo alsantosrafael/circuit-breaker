1341953b89ec71d648ef18db35d7883c87333760
Rafael Almeida <rafael.profeng@gmail.com>
2025-10-01 09:05:25 -0300

    fix: Allow test after reaching HALF_OPEN state and add javadoc

--- src/main/java/org/com/circuitbreaker/CircuitBreaker.java
+++ src/main/java/org/com/circuitbreaker/CircuitBreaker.java
@@ -1,40 +1,61 @@
 package org.com.circuitbreaker;
 
+/**
+ * Implements a simple Circuit Breaker pattern.
+ */
 public class CircuitBreaker {
 	private CircuitStatus status = CircuitStatus.CLOSED;
-
 	private int failureCounter = 0;
 	private final int failureThreshold;
-	private final long retryTimeoutMillis;
+	private final long retryTimeOutMillis;
 	private long lastFailureTime = 0;
-	public CircuitBreaker(int failureThreshold, long retryTimeoutMillis) {
+
+	/**
+	 * Constructs a CircuitBreaker with the specified failure threshold and retry timeout.
+	 *
+	 * @param failureThreshold the number of failures before opening the circuit
+	 * @param retryTimeOutMillis the timeout in milliseconds before allowing a retry
+	 */
+	public CircuitBreaker(int failureThreshold, long retryTimeOutMillis) {
 		this.failureThreshold = failureThreshold;
-		this.retryTimeoutMillis = retryTimeoutMillis;
+		this.retryTimeOutMillis = retryTimeOutMillis;
 	}
 
+	/**
+	 * Determines if a request is allowed based on the current circuit status.
+	 * If the circuit is open and the retry timeout has passed, the status is set to HALF_OPEN.
+	 *
+	 * @return true if the request is allowed, false otherwise
+	 */
 	public boolean allowRequest() {
-		if(this.status == CircuitStatus.OPEN) {
-			if(System.currentTimeMillis() - lastFailureTime > retryTimeoutMillis) {
-				status = CircuitStatus.HALF_OPEN;
+		if (this.status == CircuitStatus.OPEN) {
+			if (System.currentTimeMillis() - lastFailureTime > retryTimeOutMillis) {
+				this.status = CircuitStatus.HALF_OPEN;
 				return true;
 			}
-
 			return false;
 		}
 		return true;
 	}
 
+	/**
+	 * Records a successful request, closing the circuit and resetting counters.
+	 */
 	public void recordSuccess() {
-		this.lastFailureTime = 0;
+		this.status = CircuitStatus.CLOSED;
 		this.failureCounter = 0;
-		status = CircuitStatus.CLOSED;
+		this.lastFailureTime = 0;
 	}
 
+	/**
+	 * Records a failed request, incrementing the failure counter and updating the last failure time.
+	 * Opens the circuit if the failure threshold is reached.
+	 */
 	public void recordFailure() {
-		this.lastFailureTime = System.currentTimeMillis();
 		this.failureCounter++;
+		this.lastFailureTime = System.currentTimeMillis();
 
-		if(this.failureCounter == this.failureThreshold) {
+		if (this.failureCounter == this.failureThreshold) {
 			this.status = CircuitStatus.OPEN;
 		}
 	}
