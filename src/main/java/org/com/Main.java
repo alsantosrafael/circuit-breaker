47d1dead3f07a66b37df06c546a301916bf78984
Rafael Almeida <rafael.profeng@gmail.com>
2025-10-01 09:22:59 -0300

    chore: Test circuit breaker

--- src/main/java/org/com/Main.java
+++ src/main/java/org/com/Main.java
@@ -1,17 +1,27 @@
 package org.com;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-	public static void main(String[] args) {
-		//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-		// to see how IntelliJ IDEA suggests fixing it.
-		System.out.printf("Hello and welcome!");
+import org.com.circuitbreaker.CircuitBreaker;
 
-		for (int i = 1; i <= 5; i++) {
-			//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-			// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-			System.out.println("i = " + i);
+public class Main {
+	public static void main(String[] args) throws InterruptedException {
+		CircuitBreaker cb = new CircuitBreaker(3, 5000);
+
+		for(int i=0; i < 10; i++) {
+			System.out.println("---- Request " + i + " ----");
+			if(cb.allowRequest()) {
+				boolean success = Math.random() > 0.9;
+
+				if(success) {
+					cb.recordSuccess();
+					System.out.println("Service call SUCCESS");
+				} else {
+					cb.recordFailure();
+					System.out.println("Service call FAILURE");
+				}
+			} else {
+				System.out.println("Request blocked (Circuit is OPEN).");
+			}
+			Thread.sleep(500);
 		}
 	}
 }
